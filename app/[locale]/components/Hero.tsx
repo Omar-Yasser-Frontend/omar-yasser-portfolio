f6a7d9dd5e0a6e7af5cf43c44bbb78036d30c1ab
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-07-22 16:15:19 +0300

    fixing position of blurred circle

--- app/[locale]/components/Hero.tsx
+++ app/[locale]/components/Hero.tsx
@@ -4,7 +4,7 @@ import HeroText from './HeroText';
 function Hero() {
   return (
     <div className="relative">
-      <div className="bg-accent absolute top-1/3 right-1/4 -z-100 h-80 w-80 -translate-y-1/2 rounded-full" />
+      <div className="bg-accent absolute inset-e-1/4 top-1/3 -z-100 h-80 w-80 -translate-y-1/2 rounded-full" />
       <section className="glass-effect flex min-h-screen items-center justify-between pt-34 pb-5 lg:pt-0">
         <div className="container mx-auto grid grid-cols-1 items-center gap-3 px-4 lg:grid-cols-2">
           <HeroText />
