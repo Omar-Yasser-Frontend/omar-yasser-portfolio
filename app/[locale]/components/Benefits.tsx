80d7dac7048cfd5439be0076d19ccb1a95eab447
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-07-26 21:55:02 +0300

    feat: adding benefits section

---
+++ app/[locale]/components/Benefits.tsx
@@ -0,0 +1,65 @@
+import { useTranslations } from 'next-intl';
+import Section from './Section';
+import BenefitsCard from './BenefitsCard';
+import { Link } from '@/i18n/navigation';
+import cloud from '@/app/[locale]/assets/dns-svgrepo-com.svg';
+import frontend from '@/app/[locale]/assets/web-optimization-website-browser-svgrepo-com.svg';
+import backend from '@/app/[locale]/assets/server-square-svgrepo-com.svg';
+
+const benefitsDetails = [
+  {
+    label: 'frontend',
+    url: frontend,
+  },
+  {
+    label: 'backend',
+    url: backend,
+  },
+  {
+    label: 'cloud',
+    url: cloud,
+  },
+];
+
+function Benefits() {
+  const t = useTranslations('benefits');
+
+  return (
+    <Section>
+      <Section.Container>
+        <Section.HeadingWrapper>
+          <Section.Heading>{t('heading')}</Section.Heading>
+
+          <Section.SubHeading>{t('desc')}</Section.SubHeading>
+        </Section.HeadingWrapper>
+
+        <div className="grid gap-6 lg:gap-10 grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
+          {benefitsDetails.map(({ label, url }) => (
+            <BenefitsCard
+              url={url}
+              altName={label}
+              heading={t(`${label}.heading`)}
+              key={label}
+              desc={t.rich(`${label}.desc`, {
+                highlight: (chunk) => (
+                  <span className="text-muted font-semibold">{chunk}</span>
+                ),
+                direction: (chunk) => <bdi>{chunk}</bdi>,
+                guidelines: (chunk) => (
+                  <Link
+                    href={`/benefits#${label}`}
+                    className="visited:text-primary text-blue-600"
+                  >
+                    {chunk}
+                  </Link>
+                ),
+              })}
+            />
+          ))}
+        </div>
+      </Section.Container>
+    </Section>
+  );
+}
+
+export default Benefits;
