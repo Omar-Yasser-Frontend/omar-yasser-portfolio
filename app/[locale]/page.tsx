ac4e2dfde4082825ffcb19faab9f8b34636b5f00
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-08-02 20:47:27 +0300

    update: moving root layout into /[locale] and adding features component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,15 @@
+import Benefits from './components/Benefits';
+import Features from './components/Features';
 import Hero from './components/Hero';
 
 function page() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Benefits />
+      <Features />
+    </>
+  );
 }
 
 export default page;
