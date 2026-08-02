4f42530dc12e50485b72df182b5f39fcfb273a2b
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-08-02 20:49:24 +0300

    changing background to fixed blob glass effect

---
+++ app/[locale]/components/BlobBg.tsx
@@ -0,0 +1,11 @@
+function BlobBg() {
+  return (
+    <div className="scene" aria-hidden="true">
+      {/* <div className="scene__blob scene__blob--1"></div> */}
+      <div className="scene__blob scene__blob--2"></div>
+      <div className="scene__blob scene__blob--3"></div>
+    </div>
+  );
+}
+
+export default BlobBg;
