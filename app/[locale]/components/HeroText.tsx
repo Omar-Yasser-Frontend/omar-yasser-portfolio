5324c80a4591805fd82edfd34026c915d5299157
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-08-02 20:55:39 +0300

    feat: removing hero's background and riching i18n

--- app/[locale]/components/HeroText.tsx
+++ app/[locale]/components/HeroText.tsx
@@ -7,7 +7,9 @@ function HeroText() {
   return (
     <div className="order-2 mx-auto flex max-w-137.5 flex-col gap-4 text-center lg:order-1 lg:mx-0 lg:text-start">
       <h1 className="gradient-text text-2xl font-extrabold lg:text-4xl">
-        {t('title')}
+        {t.rich('title', {
+          en: (enWord) => <span className="en-mono">{enWord}</span>,
+        })}
       </h1>
       <hr className="text-primary mx-auto w-1/2 max-w-full lg:mx-0 lg:w-10" />
       <p className="text-muted text-sm leading-relaxed">{t('desc')}</p>
